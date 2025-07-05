c94421dba22bb8243f04a04e56937fd30cdf33c0
praveenchandhar <praveenchandhar1974@gmail.com>
2025-07-05 18:35:03 +0530

    Update liquibase_runner.sh

--- scripts/liquibase_runner.sh
+++ scripts/liquibase_runner.sh
@@ -22,7 +22,7 @@ case "$command" in
         java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
             --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority&tls=true" \
             --logLevel=debug \
-            --changeLogFile=changelog.xml \
+            --changeLogFile=changeset/changelog.xml \
             status
         ;;
     update)
@@ -30,7 +30,7 @@ case "$command" in
         java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
             --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority&tls=true" \
             --logLevel=debug \
-            --changeLogFile=changelog.xml \
+            --changeLogFile=changeset/changelog.xml \
             update
         ;;
     *)
