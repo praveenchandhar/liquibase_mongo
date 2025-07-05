c46564379317160a5cdb92b71f6cca00f50a2d3c
praveenchandhar <praveenchandhar1974@gmail.com>
2025-07-05 20:06:37 +0530

    Update liquibase_runner.sh

--- scripts/liquibase_runner.sh
+++ scripts/liquibase_runner.sh
@@ -17,25 +17,50 @@ database="$2"
 JARS_DIR=$HOME/liquibase-jars
 
 # Setup Liquibase CLASSPATH
-export CLASSPATH=$(find $JARS_DIR -name "*.jar" | tr '\n' ':')
+# Prioritize commons-lang3 and other essential jars first
+CLASSPATH="$JARS_DIR/commons-lang3.jar:$JARS_DIR/liquibase-core.jar:$JARS_DIR/liquibase-mongodb.jar:$JARS_DIR/jackson-annotations.jar:$JARS_DIR/jackson-core.jar:$JARS_DIR/jackson-databind.jar:$JARS_DIR/mongodb-driver-sync.jar:$JARS_DIR/mongodb-driver-core.jar:$JARS_DIR/bson.jar:$JARS_DIR/commons-io.jar:$JARS_DIR/snakeyaml.jar:$JARS_DIR/slf4j-api.jar:$JARS_DIR/slf4j-simple.jar:$JARS_DIR/opencsv.jar"
 
+export CLASSPATH
+
+echo "--------------- DEBUGGING START ---------------"
+echo "CLASSPATH=${CLASSPATH}" | tr ':' '\n'
+echo "Files in JARS_DIR ($JARS_DIR):"
+ls -l "$JARS_DIR"
+echo "--------------- DEBUGGING END -----------------"
+
+# Check if commons-lang3 is included in dependencies
+if ! jar tf "$JARS_DIR/commons-lang3.jar" | grep -q "org/apache/commons/lang3/SystemProperties.class"; then
+    echo "Error: commons-lang3.jar does not contain SystemProperties class."
+    exit 1
+fi
+
+# Validate that Liquibase is accessible
+echo "Validating Liquibase binary..."
+java -cp "$CLASSPATH" liquibase.integration.commandline.Main --version || {
+    echo "Liquibase execution failed: please check dependencies."
+    exit 1
+}
+
+# Command Execution
 case "$command" in
     status)
-        echo "Running Liquibase status for database: '$database'"
+        echo "Running Liquibase status for database: '$database'..."
         java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
+            status \
             --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority&tls=true" \
             --logLevel=debug \
-            --changeLogFile=changeset/changelog.xml \
-            status
+            --changeLogFile=changeset/changelog.xml
         ;;
+
     update)
-        echo "Running Liquibase update for database: '$database'"
+        echo "Running Liquibase update for database: '$database'..."
         java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
+            update \
             --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority&tls=true" \
             --logLevel=debug \
-            --changeLogFile=changeset/changelog.xml \
-            update
+            --changeLogFile=changeset/changelog.xml
         ;;
+
     *)
         echo "Unknown command: $command"
         exit 1
