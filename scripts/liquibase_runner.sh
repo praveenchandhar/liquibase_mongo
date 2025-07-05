a52b97c68271ec6ce54fa37aff78d5ce7079875b
praveenchandhar <praveenchandhar1974@gmail.com>
2025-07-05 20:26:01 +0530

    Update liquibase_runner.sh

--- scripts/liquibase_runner.sh
+++ scripts/liquibase_runner.sh
@@ -13,53 +13,31 @@ fi
 command="$1"
 database="$2"
 
-# Prepare Workspace and Dependencies
-JARS_DIR=$HOME/liquibase-jars
-CLASSPATH="$JARS_DIR/commons-lang3.jar:$JARS_DIR/liquibase-core.jar:$JARS_DIR/liquibase-mongodb.jar:$JARS_DIR/jackson-annotations.jar:$JARS_DIR/jackson-core.jar:$JARS_DIR/jackson-databind.jar:$JARS_DIR/snakeyaml.jar:$JARS_DIR/mongodb-driver-sync.jar:$JARS_DIR/mongodb-driver-core.jar:$JARS_DIR/bson.jar:$JARS_DIR/slf4j-api.jar:$JARS_DIR/slf4j-simple.jar:$JARS_DIR/commons-io.jar:$JARS_DIR/opencsv.jar"
+# Setup CLASSPATH for Liquibase dependencies
+CLASSPATH=$(find "$HOME/liquibase-jars" -name "*.jar" | tr '\n' ':')
 
-export CLASSPATH
+# Print classpath for debugging
+echo "Using classpath: $CLASSPATH"
 
-echo "--------------- DEBUGGING START ----------------"
-echo "COMMAND: $command"
-echo "DATABASE: $database"
-echo "CLASSPATH:"
-echo $CLASSPATH | tr ':' '\n'
-echo "Files in JARS_DIR:"
-ls -l $JARS_DIR
-echo "Connection URL: $MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority&tls=true"
-echo "--------------- DEBUGGING END ------------------"
+# Common Liquibase options
+LIQUIBASE_OPTS=(
+  --url="${MONGO_CONNECTION_BASE}/${database}?retryWrites=true&w=majority&tls=true"
+  --logLevel=debug
+  --changeLogFile=changeset/changelog.xml
+)
 
-# Validate Liquibase Binary
-echo "Validating Liquibase binary execution..."
-java -cp "$CLASSPATH" liquibase.integration.commandline.Main --version || {
-    echo "Liquibase failed to execute. Please check dependencies and binary."
-    exit 1
-}
-
-# Ensure changelog file exists
-if [ ! -f changeset/changelog.xml ]; then
-    echo "Error: changelog file does not exist at changeset/changelog.xml"
-    exit 1
-fi
-
-# Command Execution
+# Execute the appropriate Liquibase command
 case "$command" in
-    status)
-        echo "Running Liquibase status for database: '$database'..."
-        java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
-            status \
-            --changeLogFile=changeset/changelog.xml \
-            --url="mongodb+srv://praveenchandharts:kixIUsDWGd3n6w5S@praveen-mongodb-github.lhhwdqa.mongodb.net/$database?retryWrites=true&w=majority&tls=true"
-        ;;
-    update)
-        echo "Running Liquibase update for database: '$database'..."
-        java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
-            update \
-            --changeLogFile=changeset/changelog.xml \
-            --url="mongodb+srv://praveenchandharts:kixIUsDWGd3n6w5S@praveen-mongodb-github.lhhwdqa.mongodb.net/$database?retryWrites=true&w=majority&tls=true"
-        ;;
-    *)
-        echo "Unknown command: $command"
-        exit 1
-        ;;
+  status)
+    echo "Running Liquibase status for database: $database"
+    java -cp "$CLASSPATH" liquibase.integration.commandline.Main "${LIQUIBASE_OPTS[@]}" status
+    ;;
+  update)
+    echo "Running Liquibase update for database: $database"
+    java -cp "$CLASSPATH" liquibase.integration.commandline.Main "${LIQUIBASE_OPTS[@]}" update
+    ;;
+  *)
+    echo "Unknown command: $command"
+    exit 1
+    ;;
 esac
