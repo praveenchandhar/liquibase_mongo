991b4fb3f52a71f77cf2411169949023ae346b1e
praveenchandhar <praveenchandhar1974@gmail.com>
2025-07-05 19:28:12 +0530

    Update liquibase_runner.sh

--- scripts/liquibase_runner.sh
+++ scripts/liquibase_runner.sh
@@ -14,27 +14,30 @@ command="$1"
 database="$2"
 
 # Setup CLASSPATH for Liquibase dependencies
-CLASSPATH=$(find $HOME/liquibase-jars -name "*.jar" | tr '\n' ':')
+CLASSPATH=$(find "$HOME/liquibase-jars" -name "*.jar" | tr '\n' ':')
 
+# Print classpath for debugging
+echo "Using classpath: $CLASSPATH"
+
+# Common Liquibase options
+LIQUIBASE_OPTS=(
+  --url="${MONGO_CONNECTION_BASE}/${database}?retryWrites=true&w=majority&tls=true"
+  --logLevel=debug
+  --changeLogFile=changeset/changelog.xml
+)
+
+# Execute the appropriate Liquibase command
 case "$command" in
-    status)
-        echo "Running Liquibase status for database: $database"
-        java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
-            --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority&tls=true" \
-            --logLevel=debug \
-            --changeLogFile=changeset/changelog.xml \
-            status
-        ;;
-    update)
-        echo "Running Liquibase update for database: $database"
-        java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
-            --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority&tls=true" \
-            --logLevel=debug \
-            --changeLogFile=changeset/changelog.xml \
-            update
-        ;;
-    *)
-        echo "Unknown command: $command"
-        exit 1
-        ;;
+  status)
+    echo "Running Liquibase status for database: $database"
+    java -cp "$CLASSPATH" liquibase.integration.commandline.Main "${LIQUIBASE_OPTS[@]}" status
+    ;;
+  update)
+    echo "Running Liquibase update for database: $database"
+    java -cp "$CLASSPATH" liquibase.integration.commandline.Main "${LIQUIBASE_OPTS[@]}" update
+    ;;
+  *)
+    echo "Unknown command: $command"
+    exit 1
+    ;;
 esac
