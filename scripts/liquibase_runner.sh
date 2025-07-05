ff3a3978f1964b36e77b313b713d28a60b88f073
praveenchandhar <praveenchandhar1974@gmail.com>
2025-07-05 18:26:24 +0530

    Update liquibase_runner.sh

--- scripts/liquibase_runner.sh
+++ scripts/liquibase_runner.sh
@@ -20,7 +20,7 @@ case "$command" in
     status)
         echo "Running Liquibase status for database: $database"
         java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
-            --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority" \
+            --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority&tls=true" \
             --logLevel=debug \
             --changeLogFile=changelog.xml \
             status
@@ -28,7 +28,7 @@ case "$command" in
     update)
         echo "Running Liquibase update for database: $database"
         java -cp "$CLASSPATH" liquibase.integration.commandline.Main \
-            --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority" \
+            --url="$MONGO_CONNECTION_BASE/$database?retryWrites=true&w=majority&tls=true" \
             --logLevel=debug \
             --changeLogFile=changelog.xml \
             update
