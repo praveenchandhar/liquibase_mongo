7c89024a04bdfd1c6370bafe654f7b88175f87bc
praveenchandhar <praveenchandhar1974@gmail.com>
2025-07-25 18:46:37 +0530

    Create version_6.js

---
+++ db_queries/version_6.js
@@ -0,0 +1,14 @@
+// context: liquibase_test
+// This file contains changes for the liquibase_test database
+
+db.dropCollection("Liquibase_Demo");
+
+db.dropCollection("demo_collection");
+
+db.dropCollection("liquibase_demo");
+
+db.dropCollection("sample_collection");
+
+db.dropCollection("sample_collection_1");
+
+db.dropCollection("sample_collection_2");
